e49e7e6dd8310b9dae4ce5100d9f2e81c53d425e
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2025-06-04 22:30:06 -0300

    Add files via upload

--- pacote_sits1.R
+++ pacote_sits1.R
@@ -3,6 +3,9 @@
 options(timeout = 300)
 devtools::install_github("e-sensing/sitsdata")
 
+# usethis::create_github_token()
+# gitcreds::gitcreds_set()
+
 library(RStoolbox)
 library(sits)
 library(sitsdata)

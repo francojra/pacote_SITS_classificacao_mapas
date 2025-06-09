e827fdba80310e91295375ef8ef02a3ac60560da
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2025-06-09 20:37:54 -0300

    Add files via upload

--- pacote_sits2.R
+++ pacote_sits2.R
@@ -1,11 +1,26 @@
 
-# Baixar e carregar pacote sitsdata
+# sits: Satellite Image Time Series Analysis on Earth Observation Data Cubes ---------------------------------------------------------------
+
+# Baixar e carregar pacote sitsdata --------------------------------------------------------------------------------------------------------
 
 library(devtools) # Necessário para baixar o pacote
 options(download.file.method = "wininet")
 devtools::install_github("e-sensing/sitsdata")
 
-# Baixar pacotes para análises
+# Baixar pacotes para análises -------------------------------------------------------------------------------------------------------------
 
-library(sitsdata)
+# load package "tibble"
+library(tibble)
+# load packages "sits" and "sitsdata"
 library(sits)
+library(sitsdata)
+
+# Create a data cube using local files ---------------------------------------------------------------------------------------------------
+
+sinop_cube <- sits_cube(
+  source = "BDC",
+  collection = "MOD13Q1-6.1",
+  bands = c("NDVI", "EVI"),
+  data_dir = system.file("extdata/sinop", package = "sitsdata"),
+  parse_info = c("satellite", "sensor", "tile", "band", "date")
+)

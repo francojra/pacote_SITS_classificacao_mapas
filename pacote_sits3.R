7f48c77200588658624b73e8379c691678bc07d7
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2025-06-19 22:18:00 -0300

    Add files via upload

--- pacote_sits3.R
+++ pacote_sits3.R
@@ -141,17 +141,13 @@ plot(cube_s1_rtc, band = "VV", palette = "Greys")
 
 # Copernicus DEM 30 meter images in MPC ----------------------------------------------------------------------------------------------------
 
-roi <- c(
-  lon_min = -47.6000, lat_min = -14.2000,
-  lon_max = -47.5000, lat_max = -14.1000
-)
-
 cube_dem_30 <- sits_cube(
   source = "MPC",
   collection = "COP-DEM-GLO-30",
-  #tiles = "20LMR",
-  roi = roi,
+  tiles = "20LMR",
   band = "ELEVATION"
 )
 
 plot(cube_dem_30, band = "ELEVATION", palette = "RdYlGn", rev = TRUE)
+
+sits:sits_bbox(cube_dem_30)

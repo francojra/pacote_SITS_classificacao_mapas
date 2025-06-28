a97c6dbd55b502f4b62eb6313e10cdf22d121e4f
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2025-06-27 23:08:51 -0300

    Add files via upload

--- pacote_sits_testes.R
+++ pacote_sits_testes.R
@@ -27,35 +27,6 @@ model <- sits_train(
   ml_method = sits_rfor()  # Pode usar outros métodos
 )
 
-# 4. Preparar dados raster 
-# Baixar imagens gratuitas
-
-# Configure primeiro o acesso ao BDC (se ainda não fez)
-
-library(rstac)
-
-# 1. Primeiro configurar a fonte STAC do BDC
-source_add_stac(
-  name = "BDC",
-  url = "https://brazildatacube.dpi.inpe.br/stac/",
-  service_type = "STAC"
-)
-
-sourcetools::
-
-# 2. Criar cubo SITS para o BDC
-cerrado_bbox <- c(-53.0, -18.0, -45.0, -12.0)  # Exemplo Cerrado
-
-cube_cerrado <- sits_cube(
-  source = "STAC",
-  url = "https://brazildatacube.dpi.inpe.br/stac/",
-  collection = "S2-L2A_10_16D_STK-1",
-  bands = c("B04", "B08"),
-  bbox = cerrado_bbox,
-  start_date = "2023-01-01",
-  end_date = "2023-06-30"
-)
-
 # 4. Classificar imagem (gerar cubo de probabilidades)
 # (Substitua 'cube' pelo seu cubo de dados real)
 data("cerrado_2classes")
@@ -67,28 +38,11 @@ probs_cube <- sits_classify(
 class(probs_cube)
 View(probs_cube)
 
-# 3. Aplicar suavização no cubo de probabilidades
-ts_smoothed <- sits_smooth(
-  cube = probs_cube,
-  window_size = 9,
-)
-
-# Calcular índices adicionais (exemplo: NDVI)
-ts_processed <- sits_apply(
-  probs_cube,
-  NDVI = (nir - red) / (nir + red)
-)
-
-# 4. Treinar o modelo
-model <- sits_train(
-  samples      = ts_processed,
-  ml_method    = sits_rfor()  # Random Forest
-)
-
 # 5. Carregar dados de imagem para classificação (substitua por seu cubo de dados)
 # Exemplo usando dados MODIS
-data("modis_cube")
-cube <- modis_cube
+sits::samples_modis_ndvi
+data("samples_modis_ndvi")
+cube <- samples_modis_ndvi
 
 # 6. Classificação da imagem
 probs_cube <- sits_classify(

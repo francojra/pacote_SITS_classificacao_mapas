e7774d7d0038dfcbfe400298a438fd68194e7d61
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2025-06-30 23:12:08 -0300

    Add files via upload

---
+++ data_visualization_sits.R
@@ -0,0 +1,17 @@
+# Satellite Image Time Series Analysis on Earth Observation Data Cubes ---------------------------------------------------------------------
+# Autora do script: Jeanne Franco ----------------------------------------------------------------------------------------------------------
+# Data do script: 30/06/2025 ---------------------------------------------------------------------------------------------------------------
+# Referência: https://e-sensing.github.io/sitsbook/ ----------------------------------------------------------------------------------------
+
+# Data visualisation in SITS ---------------------------------------------------------------------------------------------------------------
+
+## Configurations to run this chapter
+
+library(sits)
+library(sitsdata)
+tempdir_visualization_sits <- "pRaticando-Software-R"
+dir.create(tempdir_visualization_sits, showWarnings = FALSE, recursive = TRUE)
+
+# Plotting ---------------------------------------------------------------------------------------------------------------------------------
+
+

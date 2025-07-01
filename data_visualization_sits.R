501ef6d95768a22f7161d5058ea6fa4a97beff8b
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2025-06-30 23:18:54 -0300

    Add files via upload

--- data_visualization_sits.R
+++ data_visualization_sits.R
@@ -14,4 +14,28 @@ dir.create(tempdir_visualization_sits, showWarnings = FALSE, recursive = TRUE)
 
 # Plotting ---------------------------------------------------------------------------------------------------------------------------------
 
+## The plot() function produces a graphical display of data cubes, 
+## time series, models, and SOM maps.
+## he plotting of time series, models, and SOM outputs uses 
+## the ggplot2 package; maps are plotted using the tmap package. 
+## When plotting images and classified maps, users can control 
+## the output, with appropriate parameters for each type of image.
 
+
+# set the directory where the data is 
+data_dir <- system.file("extdata/Rondonia-20LMR", package = "sitsdata")
+
+# read the data cube
+ro_20LMR <- sits_cube(
+  source = "MPC", 
+  collection = "SENTINEL-2-L2A",
+  data_dir = data_dir
+)
+
+# plot the NDVI for date 2022-08-01
+plot(ro_20LMR, 
+     band = "NDVI", 
+     date = "2022-08-01", 
+     palette = "Greens",
+     legend_position = "outside",
+     scale = 1.0)

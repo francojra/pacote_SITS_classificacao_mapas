30f80c9d54707721e0f2c4f3fa6e1451e7b9a622
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2025-07-01 23:19:13 -0300

    Add files via upload

---
+++ sits_real_examples.R
@@ -0,0 +1,16 @@
+# Satellite Image Time Series Analysis on Earth Observation Data Cubes ---------------------------------------------------------------------
+# Autora do script: Jeanne Franco ----------------------------------------------------------------------------------------------------------
+# Data do script: 01/07/2025 ---------------------------------------------------------------------------------------------------------------
+# Referência: https://e-sensing.github.io/sitsbook/ ----------------------------------------------------------------------------------------
+
+# How to use SITS with real examples -------------------------------------------------------------------------------------------------------
+
+# The following scripts show examples of how to use sits for 
+# land classification. The first is a classification of a 
+# Sentinel-2 tile in Rondonia (Brasil) to detect deforestation 
+# and the second a land use and land cover classification of 
+# area in the Cerrado biome in Brazil.
+
+# Configuration to run the examples in this chapter ----------------------------------------------------------------------------------------
+
+

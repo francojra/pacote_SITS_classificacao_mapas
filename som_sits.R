3b2f402e416e6897305d2256e987e797d3bb717a
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2025-07-26 00:03:31 -0300

    Add files via upload

--- som_sits.R
+++ som_sits.R
@@ -49,6 +49,19 @@ samples_cerrado_mod13q1_2bands <- sits_select(
 # Show the summary of the samples
 summary(samples_cerrado_mod13q1_2bands)
 
+# Criando o mapa SOM -----------------------------------------------------------------------------------------------------------------------
+
+# Clustering time series using SOM
+som_cluster <- sits_som_map(samples_cerrado_mod13q1_2bands,
+    grid_xdim = 15,
+    grid_ydim = 15,
+    alpha = 1.0,
+    distance = "dtw",
+    rlen = 20
+)
+
+# Plot the SOM map
+plot(som_cluster)
 
 
 
